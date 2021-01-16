1645438b623a5ec27b55c047148fe7c633522e1c
AlbaSS18 <albaserenasuarez1998@gmail.com>
2021-01-16 16:44:03 +0100

    Fix test

--- src/app/edit-user/edit-user.component.spec.ts
+++ src/app/edit-user/edit-user.component.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditUserComponent } from './edit-user.component';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TranslateModule} from '@ngx-translate/core';
+import {ButtonModule} from 'primeng/button';
+import {MessagesModule} from 'primeng/messages';
+import {InputNumberModule} from 'primeng/inputnumber';
+import {MenuBarComponent} from '../menu-bar/menu-bar.component';
+import {RouterModule} from '@angular/router';
+import {DropdownModule} from 'primeng/dropdown';
 
 describe('EditUserComponent', () => {
   let component: EditUserComponent;
@@ -8,7 +17,18 @@ describe('EditUserComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EditUserComponent ]
+      declarations: [ EditUserComponent, MenuBarComponent ],
+      imports: [
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+        FormsModule,
+        TranslateModule.forRoot(),
+        ButtonModule,
+        MessagesModule,
+        InputNumberModule,
+        RouterModule.forRoot([]),
+        DropdownModule
+      ]
     })
     .compileComponents();
   });
