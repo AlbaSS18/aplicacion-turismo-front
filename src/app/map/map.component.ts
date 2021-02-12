bfb03492596b089efd4f082087972cec3a9c1580
AlbaSS18 <albaserenasuarez1998@gmail.com>
2021-02-12 23:21:15 +0100

    Height modified

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -13,13 +13,21 @@ export class MapComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.initMap();
   }
 
   private initMap(): void {
     this.map = L.map('map', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
+      center: [ 43.333333, -6 ],
+      zoom: 8
     });
+
+    const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
   }
 
 }
