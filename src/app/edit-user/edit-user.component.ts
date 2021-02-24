18e0590811566713cdb97ca095de5fbaf542579b
AlbaSS18 <albaserenasuarez1998@gmail.com>
2021-02-24 22:37:38 +0100

    Fix bug with null in edit-user

--- src/app/edit-user/edit-user.component.ts
+++ src/app/edit-user/edit-user.component.ts
@@ -135,7 +135,7 @@ export class EditUserComponent implements OnInit {
       }
       else if (propName === "dateBirthday"){
         var d1 = new Date(new Date(a[propName]).toDateString());
-        var d2 = new Date(b[propName].toDateString());
+        var d2 = new Date(b[propName]?.toDateString());
         if (d1.getTime() !== d2.getTime()){
           this.valueUnchanged = false;
           return false;
