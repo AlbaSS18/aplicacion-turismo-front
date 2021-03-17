61b36dac4b44a51168d34de55a44c9299a62e44d
AlbaSS18 <albaserenasuarez1998@gmail.com>
2021-03-17 23:41:34 +0100

    Method to convert data64 to file

--- src/app/edit-activities/edit-activities.component.ts
+++ src/app/edit-activities/edit-activities.component.ts
@@ -110,11 +110,32 @@ export class EditActivitiesComponent implements OnInit {
 
       var url = 'data:' + this.activity.metadataImage.mimeType + ';base64,' + this.activity.metadataImage.data;
       this.image = this.sanitizer.bypassSecurityTrustUrl(url);
-      const blob = new Blob([this.image], { type: this.activity.metadataImage.mimeType });
 
-      this.file = new File([blob], response3.pathImage);
+      this.file = this.dataURItoBlob(url, this.activity.pathImage);
     });
   }
+
+  dataURItoBlob(dataURI: any, fileName: string): File{
+
+    // convert base64/URLEncoded data component to a file
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+      byteString = atob(dataURI.split(',')[1]);
+    else
+      byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new File([ia], fileName, {type: mimeString});
+  }
+
 
   observeChanges() {
     this.editActivitiesForm.valueChanges.subscribe((values) => {
