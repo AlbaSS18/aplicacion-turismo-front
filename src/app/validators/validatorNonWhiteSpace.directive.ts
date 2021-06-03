d375228e2483e8eef19b1522668301cc908e72f2
AlbaSS18 <albaserenasuarez1998@gmail.com>
2021-06-03 23:52:50 +0200

    Error in trim

--- src/app/validators/validatorNonWhiteSpace.directive.ts
+++ src/app/validators/validatorNonWhiteSpace.directive.ts
@@ -2,7 +2,7 @@ import {AbstractControl, ValidatorFn} from '@angular/forms';
 
 export function validadorNonwhiteSpace(): ValidatorFn {
   return (control: AbstractControl): {[key: string]: any} | null => {
-    return control.value.trim().length === 0 ? { hasWhiteSpace: true } : null;
+    return control.value?.trim().length === 0 ? { hasWhiteSpace: true } : null;
   };
 }
 
